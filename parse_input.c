dc939b63c1a2cf1fcddb1ce0fbcb3c0c6ad87f3e
blackoaz <pauloakello56@gmail.com>
2023-02-20 08:09:46 -0800

    function for tokenization of user inputs

---
+++ parse_input.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * parse_command - function for parsing user input
+ * @command: user command
+ * Return: returns array of the token
+ */
+char **parse_command(char *command)
+{
+	int i;
+	char *stkn;
+	char **arr;
+
+	stkn = strtok(command, " \n")
+	arr = malloc(sizeof(char *) * 32);
+
+	arr[0] = stkn;
+
+	i = 1;
+
+	while (stkn != NULL)
+	{
+		stkn = strtok(NULL, " \n");
+		arr[i] = stkn;
+		i++;
+	}
+	return (arr);
+}
+
